0e3155ccd959d85769a2779065eda13a441c632c
Krzysztof Głuszczyk <kris@awareful.com>
2021-02-12 10:10:19 +0100

    Add toolbar impl

--- app/src/main/java/com/krzysztofgluczyk/malapaczkarnia/OrderActivity.kt
+++ app/src/main/java/com/krzysztofgluczyk/malapaczkarnia/OrderActivity.kt
@@ -1,6 +1,9 @@
 package com.krzysztofgluczyk.malapaczkarnia
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.krzysztofgluczyk.malapaczkarnia.databinding.ActivityOrderBinding
 
@@ -15,9 +18,30 @@ class OrderActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         ActivityOrderBinding.inflate(layoutInflater).run {
             setContentView(root)
+            setSupportActionBar(toolbar)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
             intent.getStringExtra(ZAMOWIENIE_KEY)?.also {
                 zamowienie.text = it
             }
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+            R.id.navigate -> {
+                Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_navigation, menu)
+        return true
     }
 }
